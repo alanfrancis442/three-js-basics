6e3abb0ccf028f5cbf01747643849cb3aa369812
alanfrancis442 <alan.prime567@gmail.com>
2024-07-01 21:02:25 +0530

    learned how to create 3d text

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/Addons.js'
+import { FontLoader } from 'three/examples/jsm/Addons.js'
+import { TextGeometry } from 'three/examples/jsm/Addons.js'
 
 //three js needs render camera and a scene
 
@@ -8,6 +10,9 @@ const width = window.innerWidth
 
 
 const renderer = new THREE.WebGLRenderer({antialias:true})
+
+const textureLoader = new THREE.TextureLoader()
+const matcap = textureLoader.load('/textures/matcap.jpg')
 
 renderer.setSize(width,height)
 
@@ -29,30 +34,74 @@ const scene = new THREE.Scene()
 
 //creating a geometry
 
-const geo = new THREE.SphereGeometry(0.8,16,16)
-const material = new THREE.MeshBasicMaterial(
-  {
-    color:'white' ,
-    wireframe:true
+
+
+//Loading fonts
+const fontLoader = new FontLoader()
+
+fontLoader.load(
+  '/fonts/helvetiker_bold.typeface.json',
+  (font)=>{
+    const textgeo = new TextGeometry(
+      "Hello Three Js",
+      {
+        font: font,
+        size: 0.2,
+        depth: 0.1,
+        curveSegments: 8 ,
+        bevelEnabled: true,
+        bevelThickness: 0.01,
+        bevelSize: 0.003,
+        bevelOffset: 0,
+        bevelSegments: 3
+      }
+    )
+    textgeo.computeBoundingBox()
+    // console.log(textgeo.boundingBox.max.x)
+    //centerign the geo
+    textgeo.translate(
+      -(textgeo.boundingBox.max.x-0.01)*0.5,
+      -(textgeo.boundingBox.max.y-0.01)*0.5,
+      -textgeo.boundingBox.max.z*0.5,
+    )
+
+    //textgeo.center() can be used to center the text
+
+    // textgeo.translate(textgeo.boundingBox.max.x*0.5)
+    // textgeo.translate(textgeo.boundingBox.max.x*0.5)
+
+    const text = new THREE.Mesh(
+      textgeo,
+      // new THREE.MeshBasicMaterial({wireframe:true})
+      new THREE.MeshMatcapMaterial({matcap:matcap})
+    )
+    // text.position.x = -1
+    scene.add(text)
   }
 )
 
-const geo2 = new THREE.IcosahedronGeometry(0.8,2)
-const material2 = new THREE.MeshStandardMaterial({
-  color:'red',
 
-})
+const axishelper = new THREE.AxesHelper()
+scene.add(axishelper)
 
-const sphere2 = new THREE.Mesh(geo2,material2)
-scene.add(sphere2)
-
-const sphere = new THREE.Mesh(geo,material)
-// sphere.add(sphere2)
-scene.add(sphere)
-
-const light = new THREE.HemisphereLight(0xffff00,0xffff00)
-scene.add(light)
-
+const donutMaterial = new THREE.MeshMatcapMaterial({matcap:matcap})
+const donutGeo = new THREE.TorusGeometry( 0.5, 0.3, 16, 100 ); 
+for(let i =0;i<300;i++){
+  const donut = new THREE.Mesh(donutGeo,donutMaterial)
+  donut.position.set(
+    (Math.random()-0.5)*10,
+    (Math.random()-0.5)*10,
+    (Math.random()-0.5)*10
+  )
+  donut.rotation.set(
+    Math.PI*Math.random(),
+    Math.PI*Math.random(),
+    Math.PI*Math.random(),
+  )
+  let scale = Math.random()
+  donut.scale.set(scale,scale,scale)
+  scene.add(donut)
+}
 
 function animate(t=0){
   requestAnimationFrame(animate)
